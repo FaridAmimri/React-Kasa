680c2a7bc88a03973ab24718a94850dcf3293da1
Farid Amimri <farid.amimri@gmail.com>
2022-02-05 15:23:55 +0100

    ajout composant avatar page About

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,13 @@
 /** @format */
 
 import Header from '../components/header/Header'
+import Avatar from '../components/avatar/Avatar'
 
 function About() {
   return (
     <>
       <Header />
+      <Avatar />
     </>
   )
 }
