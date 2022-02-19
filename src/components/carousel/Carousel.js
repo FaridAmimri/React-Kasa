15c9040636711f36d0d5ab1d87beba7fddd4d60a
Farid Amimri <farid.amimri@gmail.com>
2022-02-19 10:50:51 +0100

    ajout cas de figure photo unique

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -6,6 +6,7 @@ import './Carousel.scss'
 function Carousel({ imgList }) {
   const imgListLength = imgList.length
   const [currImg, setCurrImg] = useState(0)
+  console.log(currImg)
 
   const nextSlide = () => {
     setCurrImg(currImg === imgListLength - 1 ? 0 : currImg + 1)
@@ -14,25 +15,37 @@ function Carousel({ imgList }) {
     setCurrImg(currImg === 0 ? imgListLength - 1 : currImg - 1)
   }
 
-  return (
-    <div className="carousel">
-      <FaChevronLeft className="left-arrow" onClick={prevSlide} />
-      <FaChevronRight className="right-arrow" onClick={nextSlide} />
-      {imgList.map((slide, index) => {
-        return (
-          <div
-            className={index === currImg ? 'slide-active' : 'slide'}
-            key={index}
-          >
-            {index === currImg && (
-              <img key={index} src={slide} alt="housing" />
-            )}
-            <span>{currImg+1}/{imgListLength}</span>
-          </div>
-        )
-      })}
-    </div>
-  )
+  if (imgListLength === 1) {
+    return (
+      <div className="carousel">
+        {imgList.map((slide, index) => {
+          return <>{index === currImg && <img src={slide} alt="housing" />}</>
+        })}
+      </div>
+    )
+  } else {
+    return (
+      <div className="carousel">
+        <FaChevronLeft className="left-arrow" onClick={prevSlide} />
+        <FaChevronRight className="right-arrow" onClick={nextSlide} />
+        {imgList.map((slide, index) => {
+          return (
+            <div
+              className={index === currImg ? 'slide-active' : 'slide'}
+              key={index}
+            >
+              {index === currImg && (
+                <img key={index} src={slide} alt="housing" />
+              )}
+              <span>
+                {currImg + 1}/{imgListLength}
+              </span>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
 }
 
 export default Carousel
