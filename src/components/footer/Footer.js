36b6701a09c3b8ae98ee4134e523c7d1846b82fb
Farid Amimri <farid.amimri@gmail.com>
2022-02-22 18:38:50 +0100

    maj path png

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.scss'
 function Footer() {
     return (
         <footer>
-        <img src="./logo-footer.png" alt="logo-kasa" />
+        <img src="/logo-footer.png" alt="logo-kasa" />
         <p>© 2022 Kasa. All rights reserved</p>
         </footer>
     )
