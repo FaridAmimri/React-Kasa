1d09ebd6bbd8a9fc22488a0818c32475e742c010
Farid Amimri <farid.amimri@gmail.com>
2022-02-22 18:11:02 +0100

    maj css responsive

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -1,9 +1,10 @@
 import './Logo.scss'
+// import logo from "./logoheader.png"
 
 function Logo() {
   return (
     <div className="logo">
-      <img src="./logo-header.png" alt="logo-kasa" />
+      <img src="/logo-header.png" alt="logo-kasa" />
     </div>
   )
 }
